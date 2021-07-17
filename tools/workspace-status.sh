ff5bab712bdd5ffbe0a35bd3eaab6fa834794a6d
Joel Jeske <joeljeske14@gmail.com>
2021-07-17 13:07:00 -0500

    chore(*): turn on release flow

--- tools/workspace-status.sh
+++ tools/workspace-status.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-VERSION_SUFFIX=""
-git diff-index --quiet HEAD
-if [ "$?" != "0" ]; then
-  VERSION_SUFFIX="-DIRTY"
+# Lookup the most recent tag from git if we are not told
+# what the version should be from .github/workflows/release.yaml
+if [ -z ${NEXT_VERSION+x} ]; then
+  VERSION_SUFFIX=""
+  git diff-index --quiet HEAD
+  if [ "$?" != "0" ]; then
+    VERSION_SUFFIX="-DIRTY"
+  fi
+
+  GIT_VERSION=$(git describe --always --tags --abbrev=12 | sed 's/^v//') # remove leading v from tags v1.2.4-abc
+  NEXT_VERSION="${GIT_VERSION}${VERSION_SUFFIX}"
 fi
 
-GIT_VERSION=$(git describe --always --tags --abbrev=12 | sed 's/^v//') # remove leading v from tags v1.2.4-abc
-
-echo "STABLE_GIT_VERSION ${GIT_VERSION}${VERSION_SUFFIX}"
+echo "STABLE_GIT_VERSION ${NEXT_VERSION}"
